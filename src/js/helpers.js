0a86baf10bee00f2befb5a0de29a4c9d5eed63f8
FIRST_NAME LAST_NAME <gouvispan@gmail.com>
2022-11-27 21:21:38 +0200

    deploy

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,8 +1,5 @@
-import { CRYPTOS_OF_INTEREST } from "./config";
-
 export const getJSON = async function (url) {
   try {
-   
     const res = await fetch(url);
     const data = await res.json();
 
@@ -12,17 +9,4 @@ export const getJSON = async function (url) {
   } catch (err) {
     throw err;
   }
-};
-
-export const filterCryptos = function (array, currency) {
-  const myCryptos = array.filter((crypto) =>
-    CRYPTOS_OF_INTEREST.some(
-      (symbol) =>
-        symbol === crypto.symbol.slice(0, 3) &&
-        crypto.symbol.slice(-currency.length) === currency &&
-        crypto.symbol.length < 8
-    )
-  );
-
-  return myCryptos;
 };
